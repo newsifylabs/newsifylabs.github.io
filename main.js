cc5f7078d636e604c54dc34f1bda95f0b6bead1b
vlbins <albinlandgreen@gmail.com>
2018-06-03 16:42:29 +0200

    js

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-
 // Fetching api key
 var apikey = config.API_KEY;
+
+var menubutton = document.getElementById("menubutton");
+
+window.onload = function() {
+  menubutton.onclick = function(){
+    myFunction();
+  };
+  function myFunction() {
+    console.log("hej");
+  }
+};
