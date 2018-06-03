671b0edaad0f256728d9b6b14f9e3f2c55db5bc0
vlbins <albinlandgreen@gmail.com>
2018-06-03 18:13:51 +0200

    sidebar

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 // Fetching api key
 var apikey = config.API_KEY;
 
-var menubutton = document.getElementById("menubutton");
 
-window.onload = function() {
-  menubutton.onclick = function(){
-    myFunction();
+window.onload = function(){
+  var sidebar = document.getElementById("sidebar");
+  sidebar.style.left = "-300px";
+
+  var menubutton = document.getElementById("menubutton");
+  menubutton.onclick = function() {
+    if(sidebar.style.left === "-300px"){
+      sidebar.style.left = "0px";
+      sidebar.style.boxShadow = "0px 0px 56px 0 rgba(0,0,0,0.32)";
+    } else {
+      sidebar.style.left = "-300px";
+      sidebar.style.boxShadow = "none";
+    }
   };
-  function myFunction() {
-    console.log("hej");
-  }
 };
